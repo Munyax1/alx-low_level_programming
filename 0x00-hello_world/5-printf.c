2f1435203d1a7de2ab5553b85b1cb8fc809ae044
Munyax1 <tonymunyax@gmail.com>
2022-06-30 13:47:33 -0700

    C program that prints exactly using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * Main : this is the entry point
+ *
+ * Description: return (0) to end program
+ *
+ */
+int main(void)
+{	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
