50892cad8df205d5165569891336a58ed1172e48
Munyax1 <tonymunyax@gmail.com>
2022-06-30 14:48:36 -0700

    C program that prints exactly using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-/*
+/**
  * Main : this is the entry point
- *
  * Description: return (0) to end program
- *
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
