306510df72a6e5d85589b78e0a36488f927beefe
Munyax1 <tonymunyax@gmail.com>
2022-07-01 13:32:46 -0700

    program that prints the alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,15 +9,10 @@
 int main(void)
 
 {
-	char low, e, q;
+	char low;
 
-	e = 'e';
-	q = 'q';
 	for (low = 'a'; low <= 'z'; low++)
-	{
-		if (low != e && low != q)
-			putchar(low);
-	}
-	putchar('\n');
+		putchar(low);
+		putchar('\n');
 	return (0);
 }
